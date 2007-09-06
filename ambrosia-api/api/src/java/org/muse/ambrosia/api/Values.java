b78aca91ef7842cf9fa2f7a767db25532690b8c7
ggolden@umich.edu <ggolden@umich.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-09-06 13:39:40 +0000

    fixed copyright
    git-svn-id: https://source.sakaiproject.org/contrib/muse/ambrosia/trunk@11772 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- ambrosia-api/api/src/java/org/muse/ambrosia/api/Values.java
+++ ambrosia-api/api/src/java/org/muse/ambrosia/api/Values.java
@@ -1,20 +1,20 @@
 /**********************************************************************************
- * $URL: $
- * $Id: $
+ * $URL$
+ * $Id$
  ***********************************************************************************
  *
- * Copyright (c) 2007 The Sakai Foundation.
+ * Copyright (c) 2007 The Regents of the University of Michigan & Foothill College, ETUDES Project
  * 
- * Licensed under the Educational Community License, Version 1.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *      http://www.opensource.org/licenses/ecl1.php
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  **********************************************************************************/
@@ -22,20 +22,22 @@
 package org.muse.ambrosia.api;
 
 /**
- * A Value can be used to capture multiple vlaues from the interface.
+ * A Value can be used to capture multiple values from the interface.
  */
 public interface Values
 {
 	/**
 	 * Access the values
+	 * 
 	 * @return array of string values
 	 */
 	public String[] getValues();
-		
+
 	/**
 	 * set the values
-	 * @param values 
-	 * 		The values
+	 * 
+	 * @param values
+	 *        The values
 	 */
 	public void setValues(String[] values);
 
